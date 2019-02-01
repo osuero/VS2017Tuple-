9cee9ec7d91fdfdeb90ce794de13816b245b6778
Octavio Augusto Suero Paniagua <octavio.suero@INE.LOCAL>
2019-02-01 10:49:20 -0400

    adding tuple examples

---
+++ Tuple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewTuple();
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void NewTuple()
+        {
+            var tupleTest = new Tuple<int, int, string>(2,2, "---->The two first were numbers----");
+
+            Console.WriteLine("Using Tuple with new \n");
+            Console.WriteLine("\n Result of tuples: {0} this is an interger type, {1} this one is also an interger type, finally this is an string type {2}", tupleTest.Item1.ToString(), tupleTest.Item2.ToString(), tupleTest.Item3);
+            Console.ReadKey();
+        }
+    }
+}
